a57a5e940fc8e37f7def61b3994391e5fb1a08f5
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2024-01-24 22:06:01 +0300

    Still not done

--- RPS/index.js
+++ RPS/index.js
@@ -1,50 +1,7 @@
-// // It's Really messy but it works!
-// // It's best out of 5 and statically done!
-
-
-
-// function compute(x,y){
-//     if (x === y){
-//         alert("Go again!")
-//     }
-//     else{
-//         if ((x === 0 && y === 1) || (x === 1 && y === 2) || (x === 2 && y === 0)){
-//             alert("Computer Wins")
-//             compwin = compwin + 1
-//         }
-//         else if((x === 0 && y === 2) || (x === 1 && y === 0) || (x === 2 && y === 1)){
-//             alert("Human Wins")
-//             humwin = humwin + 1
-//         }
-//     }
-//     return [compwin,humwin]
-// }
-
-// for (i =0; i <= 5;i++){
-//     my = parseInt(prompt("Enter Rock, Paper of Scissors?"))
-//     comp = Math.floor(Math.random() * 3)
-//     console.log(compute(my,comp))
-// }
-
-// declare(compute)
-
-// let compwin = 0
-// let humwin = 0
-// function declare(val){
-//     if (val[0] === val[1]){
-//         console.log("It's a tie")
-//     }
-//     else if(val[0] > val[1]){
-//         console.log("It's the Humans this time!")
-//     }
-//     else{
-//         console.log("It's the Computers this time!")
-//     }
-// }
-
 let compwin = 0
 let humwin = 0
 
+const result = document.querySelector('.results')
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
         console.log("Go Again!")
@@ -61,50 +18,53 @@ function playRound(playerSelection, computerSelection) {
     }
     return [humwin,compwin]
 }
-   
-//   const playerSelection = "rock";
-//   const computerSelection = getComputerChoice();
-//   console.log(playRound(playerSelection, computerSelection));
+
+const btns = document.querySelectorAll('button')
+btns.forEach(btn => btn.addEventListener('click',(e)=>myFunc(e)))
+
+function myFunc(event){
+    const playerSelection = event.target.value
+    const computerSelection = getComputerChoice()
+    playRound(playerSelection,computerSelection)
+    [compwin , humwin ]= playRound()
+    
+    if(compwin == 3 || humwin == 3){
+        if (compwin > humwin){
+            result.textContent = "Computer Wins the game!"
+        }
+        else{
+            result.textContent = "Human wins the game"
+        }
+        compwin = 0
+        humwin = 0
+    }
+}
 
 
-const choice = ["rock","paper","scissor"]
 function getComputerChoice(){
+    const choice = ["rock","paper","scissor"]
     compChoice = Math.floor(Math.random() * choice.length)
     return choice[compChoice]
 }
 
-function playerSelection(){
-    humChoice = prompt("Enter 'rock','paper',or 'scissor'")
-    if (!choice.includes(humChoice)){
-        console.log("Please Enter the specified words!")
-        playerSelection()
-    }
-    return humChoice
-}
-console.log(playerSelection() ,getComputerChoice())
+// function game(){
 
+//     x = playRound()
+//     console.log(x)
+//     if (x[0] == x[1]){
+//         console.log('It is a Tie!')
+//     }
+//     else if(x[0] < x[1]){
+//         console.log('computer Wins')
+//     }
+//     else{
+//         console.log('Human Wins')
+//     }
+    
 
+// }
 
-
-function game(){
-    for(i = 0 ; i < 5 ; i++){
-        playRound(playerSelection(),getComputerChoice())
-    }
-    x = playRound()
-    if (x[0] === x [1]){
-        console.log("It's a tie")
-    }
-    else{
-        if(x[0] > x[1]){
-            console.log("Humane wins!")
-        }
-        else{
-            console.log("Computer wins")
-        }
-    }
-}
-
-game()
+// game()
 
 
 
